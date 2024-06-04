b85f35e55d127cf6a5be02630b56ac6ac101d7a2
MGuruNikhil <gurunikhilmangaraju@gmail.com>
2024-06-04 20:20:01 +0530

    read page done

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { apiUrl } from '../config';
 import CircularProgress from '@mui/material/CircularProgress';
+import { useNavigate } from 'react-router-dom';
 
 const Feed = () => {
     const [blogs, setBlogs] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
+    const navigate = useNavigate();
 
     useEffect(() => {
         setIsLoading(true);
@@ -26,11 +28,11 @@ const Feed = () => {
     }, []);
 
     return (
-        <div className='flex flex-col overflow-auto items-center w-[80%] h-full p-2'>
+        <div className='flex flex-col overflow-auto items-center m-auto w-[80%] h-full p-2'>
             {isLoading ? <CircularProgress /> : 
                 <> 
                     {blogs.length > 0 ? blogs.map((blog, index) => (
-                        <div key={index} className='flex flex-col px-4 py-2 bg-[#69b3e3] rounded-xl'>
+                        <div key={index} onClick={() => {navigate('/read/'+blog._id)}} className='w-full flex flex-col px-4 py-2 bg-[#69b3e3] rounded-xl cursor-pointer'>
                             <h1>{blog.title}</h1>
                             <p>{blog.body.substring(0, 100)}...</p>
                             <span className='self-end'>- {blog.author}</span>
--- frontend/src/pages/ReadBlog.jsx
+++ frontend/src/pages/ReadBlog.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import PrimarySearchAppBar from '../MUIComponents.jsx/PrimarySearchAppBar'
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { apiUrl } from '../config';
+import { CircularProgress } from '@mui/material';
 
 const ReadBlog = () => {
-  return (
-    <div>
-      ReadBlog
-    </div>
-  )
+
+	const params = useParams();
+	const id = params.id;
+	const [blog, setBlog] = useState({});
+	const [isLoading, setIsLoaing] = useState(false);
+
+	useEffect(() => {
+		setIsLoaing(true);
+		axios.get(apiUrl+"blogs/"+id)
+		.then((res) => {
+			setBlog(res.data);
+			setIsLoaing(false);
+		})
+		.catch((error) => {
+			console.log(error);
+			setIsLoaing(false);
+		});
+	},[])
+
+	return (
+		<div className='w-full h-full'>
+			<PrimarySearchAppBar />
+			<div className='w-[80%] m-auto flex items-center justify-center'>
+				{isLoading ? <CircularProgress /> :
+					<div className='w-full h-full flex flex-col gap-2'>
+						<p className='text-3xl font-black'>{blog.title}</p>
+						<p>{blog.body}</p>
+						<p>Written by {blog.author}</p>
+					</div>
+				}
+			</div>
+		</div>
+	)
 }
 
 export default ReadBlog
